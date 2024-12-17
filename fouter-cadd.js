ec1404a7516a75b5e1ac2a7455b5547af91805f4
ourdesign24 <65226329+ourdesign24@users.noreply.github.com>
2024-12-17 21:29:48 +0600

    Create fouter-cadd.js

---
+++ fouter-cadd.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var footer = document.getElementById("footer-wrapper"); 
+    var originalText = "© 2024 ourislambd.com"; // Design by
+
+    
+    if (!footer || footer.innerText.trim() !== originalText) {
+       
+        window.location.href = "https://trickbd.com"; 
+    }
+});
